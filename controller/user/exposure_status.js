41347b235cf599ba11301103be40b16115a6a568
nadan <qskeksq@gmail.com>
2018-05-19 10:08:39 +0900

    exposure_status promise 테스트

--- controller/user/exposure_status.js
+++ controller/user/exposure_status.js
@@ -3,61 +3,48 @@ let logger = require('../../util/logger');
 let result = require('../../response/result');
 let error = require('../../response/error');
 
-function makeNewUserES(user_id, callback) {
-    new Promise((resolve, reject)=>{
-    
-    }).
-    then((data)=>{
-        result.send(200, "리뷰 추가 완료되었습니다", {}, res);
-    }).
-    catch((error)=>{
-        next(error);
-    });
-
-    // 처음에는 빈 데이터를 만들고 추후 update에서 따로 데이터를 받도록 하자
-    logger.debug('[2]controller-makeNewUserES');
-    model.makeNewUserES(user_id, callback);
-}
+// function makeNewUserES(user_id, callback) {
+//     new Promise((resolve, reject)=>{
+//         // 처음에는 빈 데이터를 만들고 추후 update에서 따로 데이터를 받도록 하자
+//         logger.debug('[2]controller-makeNewUserES');
+//         model.makeNewUserES(user_id, callback);
+//     }).
+//     then((data)=>{
+//         result.send(200, "", {}, res);
+//     }).
+//     catch((error)=>{
+//         next(error);
+//     });
+// }
 
 // router.get('/:user_id/exposure', exposure_status.read);
 function getUserESInfo(req, res, next) {
     new Promise((resolve, reject)=>{
-    
+        logger.debug('[2]controller-getUserESInfo');
+        // 유저 인증 해야 함
+        let user_id = req.params.user_id;
+        model.getUserESInfo(user_id, resolve, reject);
     }).
     then((data)=>{
-        result.send(200, "리뷰 추가 완료되었습니다", {}, res);
+        result.send(200, `${req.params.user_id} 회원정보 노출정보 조회가 완료되었습니다`, data, res);
     }).
     catch((error)=>{
         next(error);
-    });
-
-    logger.debug('[2]controller-getUserESInfo');
-    // 유저 인증 해야 함
-    let user_id = req.params.user_id;
-    model.getUserESInfo(user_id, (err, data)=>{
-        if(err) return next(err);
-        result.send(200, `${user_id} 회원정보 노출정보 조회가 완료되었습니다`, data, res);
     });
 }
 
 // router.put('/:user_id/exposure', exposure_status.update);
-function changeUserESInfo(req, re, next) {
+function changeUserESInfo(req, res, next) {
     new Promise((resolve, reject)=>{
-    
+        let user_id = req.params.user_id;
+        var dataObj = req.body;
+        model.changeUserESInfo(user_id, dataObj, resolve, reject);
     }).
     then((data)=>{
-        result.send(200, "리뷰 추가 완료되었습니다", {}, res);
+        result.send(200, `${req.params.user_id} 회원 정보 업데이트가 완료되었습니다`, {}, res);
     }).
     catch((error)=>{
         next(error);
-    });
-
-    // logger.debug('[2]controller-changeUserESInfo');
-    let user_id = req.params.user_id;
-    var dataObj = req.body;
-    model.changeUserESInfo(user_id, dataObj, (err, data)=>{
-        if(err) return next(err);
-        result.send(200, `${user_id} 회원 정보 업데이트가 완료되었습니다`, {}, res);
     });
 }
 
