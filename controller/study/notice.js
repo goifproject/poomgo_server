618928704f06c96ade8f80316bbc4175077974e2
nadan <qskeksq@gmail.com>
2018-05-20 02:20:54 +0900

    notice promise 테스트

--- controller/study/notice.js
+++ controller/study/notice.js
@@ -7,24 +7,19 @@ let error = require('../../response/error');
 // router.post('/:study_id/notice', notice.create);
 function makeNewNotice(req, res, next) {
     new Promise((resolve, reject)=>{
-    
+        logger.debug('[2]controller-makeNewNotice');
+        let study_id = req.params.study_id;
+        var dataObj = req.body;
+        dataObj.reg_date = new Date();
+        // TODO Datetime 클라이언트랑 협의 필요함
+        dataObj.start_date = new Date();
+        model.makeNewNotice(study_id, dataObj, resolve, reject);
     }).
     then((data)=>{
-        result.send(200, "리뷰 추가 완료되었습니다", {}, res);
+        result.send(200, `${req.params.study_id}번 스터디에 공지 생성이 완료되었습니다`, {}, res);
     }).
     catch((error)=>{
         next(error);
-    });
-
-    logger.debug('[2]controller-makeNewNotice');
-    let study_id = req.params.study_id;
-    var dataObj = req.body;
-    dataObj.reg_date = new Date();
-    // TODO Datetime 클라이언트랑 협의 필요함
-    dataObj.start_date = new Date();
-    model.makeNewNotice(study_id, dataObj, (err, data)=>{
-        if(err) return next(err);
-        result.send(200, `${study_id}번 스터디에 공지 생성이 완료되었습니다`, {}, res);
     });
 }
 
@@ -32,21 +27,16 @@ function makeNewNotice(req, res, next) {
 // router.get('/:study_id/notice/:notice_id', notice.selectSingle);
 function getNoticeInfo(req, res, next) {
     new Promise((resolve, reject)=>{
-    
+        logger.debug('[2]controller-getNoticeInfo');
+        let study_id = req.params.study_id;
+        let notice_id = req.params.notice_id;
+        model.getNoticeInfo(notice_id, resolve, reject);
     }).
     then((data)=>{
-        result.send(200, "리뷰 추가 완료되었습니다", {}, res);
+        result.send(200, `${req.params.study_id}번 스터디 ${req.params.notice_id}공지 조회가 완료되었습니다`, data, res);
     }).
     catch((error)=>{
         next(error);
-    });
-
-    logger.debug('[2]controller-getNoticeInfo');
-    let study_id = req.params.study_id;
-    let notice_id = req.params.notice_id;
-    model.getNoticeInfo(notice_id, (err, data)=>{
-        if(err) return next(err);
-        result.send(200, `${study_id}번 스터디 ${notice_id}공지 조회가 완료되었습니다`, data, res);
     });
 }
 
@@ -54,20 +44,15 @@ function getNoticeInfo(req, res, next) {
 // router.get('/:study_id/notice', notice.selectAll);
 function getNoticeInfoList(req, res, next) {
     new Promise((resolve, reject)=>{
-    
+        logger.debug('[2]controller-getNoticeInfoList');
+        let study_id = req.params.study_id;
+        model.getNoticeInfoList(study_id, resolve, reject);
     }).
     then((data)=>{
-        result.send(200, "리뷰 추가 완료되었습니다", {}, res);
+        result.send(200, `${req.params.study_id}번 스터디 공지 전체 조회가 완료되었습니다`, data, res);
     }).
     catch((error)=>{
         next(error);
-    });
-
-    logger.debug('[2]controller-getNoticeInfoList');
-    let study_id = req.params.study_id;
-    model.getNoticeInfoList(study_id, (err, data)=>{
-        if(err) return next(err);
-        result.send(200, `${study_id}번 스터디 공지 전체 조회가 완료되었습니다`, data, res);
     });
 }
 
@@ -75,24 +60,19 @@ function getNoticeInfoList(req, res, next) {
 // router.put('/:study_id/notice/:notice_id', notice.update);
 function changeNoticeInfo(req, res, next) {
     new Promise((resolve, reject)=>{
-    
+        logger.debug('[2]controller-changeNoticeInfo');
+        let study_id = req.params.study_id;
+        let notice_id = req.params.notice_id;
+        var dataObj = req.body;
+        dataObj.update_date = new Date();
+        // TODO Datetime 클라이언트랑 협의 필요함
+        model.changeNoticeInfo(notice_id, dataObj, resolve, reject);
     }).
     then((data)=>{
-        result.send(200, "리뷰 추가 완료되었습니다", {}, res);
+        result.send(200, `${req.params.study_id}번 스터디 ${req.params.notice_id} 공지 업데이트가 완료되었습니다`, {}, res);
     }).
     catch((error)=>{
         next(error);
-    });
-
-    logger.debug('[2]controller-changeNoticeInfo');
-    let study_id = req.params.study_id;
-    let notice_id = req.params.notice_id;
-    var dataObj = req.body;
-    dataObj.update_date = new Date();
-    // TODO Datetime 클라이언트랑 협의 필요함
-    model.changeNoticeInfo(notice_id, dataObj, (err, data)=>{
-        if(err) return next(err);
-        result.send(200, `${study_id}번 스터디 ${notice_id} 공지 업데이트가 완료되었습니다`, {}, res);
     });
 }
 
@@ -100,21 +80,16 @@ function changeNoticeInfo(req, res, next) {
 // router.delete('/:study_id/:notice/:notice_id', notice.delete);
 function removeNotice(req, res, next) {
     new Promise((resolve, reject)=>{
-    
+        logger.debug('[2]controller-removeNotice');
+        let study_id = req.params.study_id;
+        let notice_id = req.params.notice_id;
+        model.removeNotice(notice_id, resolve, reject);
     }).
     then((data)=>{
-        result.send(200, "리뷰 추가 완료되었습니다", {}, res);
+        result.send(200, `${req.params.study_id}번 스터디 ${req.params.notice_id}공지 삭제가 완료되었습니다`, {}, res);
     }).
     catch((error)=>{
         next(error);
-    });
-
-    logger.debug('[2]controller-removeNotice');
-    let study_id = req.params.study_id;
-    let notice_id = req.params.notice_id;
-    model.removeNotice(notice_id, (err, data)=>{
-        if(err) return next(err);
-        result.send(200, `${study_id}번 스터디 ${notice_id}공지 삭제가 완료되었습니다`, {}, res);
     });
 }
 
